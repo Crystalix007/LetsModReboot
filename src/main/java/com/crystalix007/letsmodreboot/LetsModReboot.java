fe6cea6c65d162dedd4b1408cfd8cdd4ebacb752
michaelkuc6 <michaelkuc6@gmail.com>
2014-07-28 17:19:36 +0100

    Added mod file framework
    Added a framework in which the mod can be developed and loaded using FML

---
+++ src/main/java/com/crystalix007/letsmodreboot/LetsModReboot.java
@@ -0,0 +1,10 @@
+package com.crystalix007.letsmodreboot;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = "LetsModReboot", name = "Lets Mod Reboot", version = "1.7.2-1.0")
+
+public class LetsModReboot
+{
+
+}
