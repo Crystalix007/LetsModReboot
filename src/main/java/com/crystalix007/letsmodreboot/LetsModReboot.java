90678c7f5d054b464f2f024c05301ec0f91f1566
michaelkuc6 <michaelkuc6@gmail.com>
2014-07-28 19:04:18 +0100

    Framework work and adding events
    Using subscription to the EventHandler, events were added, also
    resources framework fleshed out

--- src/main/java/com/crystalix007/letsmodreboot/LetsModReboot.java
+++ src/main/java/com/crystalix007/letsmodreboot/LetsModReboot.java
@@ -1,10 +1,32 @@
 package com.crystalix007.letsmodreboot;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "LetsModReboot", name = "Lets Mod Reboot", version = "1.7.2-1.0")
 
 public class LetsModReboot
 {
+	@Mod.Instance("LetsModReboot")
+	public static LetsModReboot instance;
 
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event)    //Network handling, mod config, items and blocks init
+	{
+
+	}
+
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event)  //Register GUIs, TileEntities, crafting recipes, general even handlers
+	{
+
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event)  //Things you want to do after other mods have loaded
+	{
+
+	}
 }
