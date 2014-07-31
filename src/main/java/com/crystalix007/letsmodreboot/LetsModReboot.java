9f19b4153ffce3aa9e185e3b6acd375bece0781f
michaelkuc6 <michaelkuc6@gmail.com>
2014-07-31 16:07:28 +0100

    Added example usage of logger utility class
    Using the new LogHelper class, it's functionality is demonstrated
    through the use of info() member function calls that notify the user
    when the FML initialization stages complete.

--- src/main/java/com/crystalix007/letsmodreboot/LetsModReboot.java
+++ src/main/java/com/crystalix007/letsmodreboot/LetsModReboot.java
@@ -2,6 +2,7 @@ package com.crystalix007.letsmodreboot;
 import com.crystalix007.letsmodreboot.handler.ConfigurationHandler;
 import com.crystalix007.letsmodreboot.proxy.IProxy;
 import com.crystalix007.letsmodreboot.reference.Reference;
+import com.crystalix007.letsmodreboot.utility.LogHelper;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -21,17 +22,18 @@ public class LetsModReboot
 	public void preInit(FMLPreInitializationEvent event)    //Network handling, mod config, items and blocks init
 	{
 		ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+		LogHelper.info("Pre-initialization complete");
 	}
 
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event)  //Register GUIs, TileEntities, crafting recipes, general even handlers
 	{
-
+		LogHelper.info("Initialization complete");
 	}
 
 	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent event)  //Things you want to do after other mods have loaded
 	{
-
+		LogHelper.info("Post-initialization complete");
 	}
 }
