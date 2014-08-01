d279a2e07b58d48dce8e46432ca6b47c675d2e77
michaelkuc6 <michaelkuc6@gmail.com>
2014-08-01 20:01:47 +0100

    Basic item completed, including image
    Using more IIconRegisters, now item icons can be set easily

--- src/main/java/com/crystalix007/letsmodreboot/item/ItemLMRB.java
+++ src/main/java/com/crystalix007/letsmodreboot/item/ItemLMRB.java
@@ -1,6 +1,9 @@
 package com.crystalix007.letsmodreboot.item;
 
 import com.crystalix007.letsmodreboot.reference.Reference;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
@@ -9,20 +12,25 @@ public class ItemLMRB extends Item
 	public ItemLMRB()
 	{
 		super();
-		this.maxStackSize = 1;
-		this.setNoRepair();
 	}
 
 	@Override
 	public String getUnlocalizedName()
 	{
-		return String.format("item.%s%s", Reference.MOD_ID.toLowerCase(), getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+		return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
 	}
 
 	@Override
 	public String getUnlocalizedName(ItemStack itemStack)
 	{
-		return String.format("item.%s%s", Reference.MOD_ID.toLowerCase(), getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+		return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister iconRegister)
+	{
+		itemIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
 	}
 
 	protected String getUnwrappedUnlocalizedName(String unlocalizedName)
