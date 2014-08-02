6d8ab122df5dbc9468cf26a2259c79337f9cf0bf
michaelkuc6 <michaelkuc6@gmail.com>
2014-08-02 14:36:22 +0100

    Added OreDictionary compatibility
    Using the ShapedOreRecipe and ShapelessOreRecipe classes,  the recipes
    now support ore dictionary registered items. Also, made flag recipe more
    realistic.

--- src/main/java/com/crystalix007/letsmodreboot/init/Recipes.java
+++ src/main/java/com/crystalix007/letsmodreboot/init/Recipes.java
@@ -4,12 +4,17 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 public class Recipes
 {
 	public static void init()
 	{
-		GameRegistry.addShapedRecipe(new ItemStack(ModItems.mapleLeaf), " X ", "XOX", " O ", 'X', new ItemStack(Blocks.leaves), 'O', new ItemStack(Items.stick));
-		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.flag), new ItemStack(ModItems.mapleLeaf), new ItemStack(ModItems.mapleLeaf));
+		//GameRegistry.addShapedRecipe(new ItemStack(ModItems.mapleLeaf), " X ", "XOX", " O ", 'X', new ItemStack(Blocks.leaves), 'O', new ItemStack(Items.stick));
+		//GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.flag), new ItemStack(ModItems.mapleLeaf), new ItemStack(ModItems.mapleLeaf));
+		//Using ore dictionary to allow mod compatibility
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.mapleLeaf), " X ", "XOX", " O ", 'X', "treeLeaves", 'O', "stickWood"));
+		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ModBlocks.flag), "stickWood", new ItemStack(ModItems.mapleLeaf)));
 	}
 }
