0280993023d0224ce14dabac7800eb2440bd2363
michaelkuc6 <michaelkuc6@gmail.com>
2014-08-02 14:19:19 +0100

    Added demo recipes recipes
    Using the GameRegistry, two example recipes were added. One is shaped:
    { X
    XOX
    O  } where 'X's are leaves and 'O's are sticks.
    Also, a shapeless recipe of two Maple leaves creates a flag block.

--- src/main/java/com/crystalix007/letsmodreboot/LetsModReboot.java
+++ src/main/java/com/crystalix007/letsmodreboot/LetsModReboot.java
@@ -2,6 +2,7 @@ package com.crystalix007.letsmodreboot;
 import com.crystalix007.letsmodreboot.handler.ConfigurationHandler;
 import com.crystalix007.letsmodreboot.init.ModBlocks;
 import com.crystalix007.letsmodreboot.init.ModItems;
+import com.crystalix007.letsmodreboot.init.Recipes;
 import com.crystalix007.letsmodreboot.proxy.IProxy;
 import com.crystalix007.letsmodreboot.reference.Reference;
 import com.crystalix007.letsmodreboot.utility.LogHelper;
@@ -28,26 +29,29 @@ public class LetsModReboot
 	{
 		ConfigurationHandler.init(event.getSuggestedConfigurationFile());
 		FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
-		LogHelper.info("Pre-initialization complete");
 
 		//Init items
 		ModItems.init();
 		//Init blocks
 		ModBlocks.init();
+
+		LogHelper.info("Pre-initialization complete");
 	}
 
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event)  //Register GUIs, TileEntities, crafting recipes, general even handlers
 	{
+		Recipes.init();
+
 		LogHelper.info("Initialization complete");
 	}
 
 	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent event)  //Things you want to do after other mods have loaded
 	{
-		LogHelper.info("Post-initialization complete");
-
 		for (String oreName : OreDictionary.getOreNames())
 			LogHelper.info("Id: "+ OreDictionary.getOreID(oreName) + ", name: " + oreName);
+
+		LogHelper.info("Post-initialization complete");
 	}
 }
---
+++ src/main/java/com/crystalix007/letsmodreboot/init/Recipes.java
@@ -0,0 +1,15 @@
+package com.crystalix007.letsmodreboot.init;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+
+public class Recipes
+{
+	public static void init()
+	{
+		GameRegistry.addRecipe(new ItemStack(ModItems.mapleLeaf), " X ", "XOX", " O ", 'X', new ItemStack(Blocks.leaves), 'O', new ItemStack(Items.stick));
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.flag), new ItemStack(ModItems.mapleLeaf), new ItemStack(ModItems.mapleLeaf));
+	}
+}
