9fa528e301f1b9c95f43c59fc25ceb4761f8d3fc
olizmc <olizmc@gmail.com>
2014-08-04 14:41:23 +0100

    Added SiNi pickaxe
    Using the same framework as the Spinel pickaxe, a SiNi pickaxe has been
    added. This is much faster, enchantable and has a far greater
    durability. Still, both are uncraftable.

--- src/main/java/com/crystalix007/letsmodreboot/init/ModItems.java
+++ src/main/java/com/crystalix007/letsmodreboot/init/ModItems.java
@@ -1,8 +1,6 @@
 package com.crystalix007.letsmodreboot.init;
 
-import com.crystalix007.letsmodreboot.item.ItemLMRB;
-import com.crystalix007.letsmodreboot.item.ItemMapleLeaf;
-import com.crystalix007.letsmodreboot.item.ItemSpinelPickaxe;
+import com.crystalix007.letsmodreboot.item.*;
 import com.crystalix007.letsmodreboot.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
@@ -14,11 +12,14 @@ public class ModItems
 {
 	public static final ItemLMRB mapleLeaf = new ItemMapleLeaf();
     public static Item.ToolMaterial spinelMaterial = EnumHelper.addToolMaterial("spinelMaterial", 4, 3000, 12, 7, 0);
-    public static final ItemPickaxe spinelPickaxe = new ItemSpinelPickaxe(spinelMaterial);
+    public static Item.ToolMaterial siliconNitrideMaterial = EnumHelper.addToolMaterial("siliconNitrideMaterial", 5, 8000, 20, 10, 30);
+    public static final ItemPickaxeLMRB spinelPickaxe = new ItemSpinelPickaxe(spinelMaterial);
+    public static final ItemPickaxeLMRB siliconNitridePickaxe = new ItemSiliconNitridePickaxe(siliconNitrideMaterial);
 
 	public static void init()
 	{
 		GameRegistry.registerItem(mapleLeaf, "mapleLeaf");
         GameRegistry.registerItem(spinelPickaxe, "spinelPickaxe");
+        GameRegistry.registerItem(siliconNitridePickaxe, "siliconNitridePickaxe");
 	}
 }
---
+++ src/main/java/com/crystalix007/letsmodreboot/item/ItemSiliconNitridePickaxe.java
@@ -0,0 +1,13 @@
+package com.crystalix007.letsmodreboot.item;
+
+import net.minecraft.block.material.Material;
+
+public class ItemSiliconNitridePickaxe extends ItemPickaxeLMRB
+{
+    public ItemSiliconNitridePickaxe(ToolMaterial toolMaterial)
+    {
+        super(toolMaterial);
+        this.setUnlocalizedName("siliconNitridePickaxe");
+        this.setMaxStackSize(1);
+    }
+}
