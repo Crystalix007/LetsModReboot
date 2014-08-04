b06ba216a1faafbd4df23d31ce2e6edfa16fcc69
michaelkuc6 <michaelkuc6@gmail.com>
2014-08-04 18:46:11 +0100

    Fixed syntax error
    Fixed syntax error caused by the absence of a right curly brace

--- src/main/java/com/crystalix007/letsmodreboot/item/ItemNewBow.java
+++ src/main/java/com/crystalix007/letsmodreboot/item/ItemNewBow.java
@@ -9,10 +9,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.entity.item.EntityBoat;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
@@ -70,76 +67,64 @@ public class ItemNewBow extends ItemBow
         return this.iconArray[duration];
     }
 
-	public void onPlayerStoppedUsing(ItemStack p_77615_1_, World p_77615_2_, EntityPlayer p_77615_3_, int p_77615_4_)
-	{
+	public void onPlayerStoppedUsing(ItemStack p_77615_1_, World p_77615_2_, EntityPlayer p_77615_3_, int p_77615_4_) {
 		int j = this.getMaxItemUseDuration(p_77615_1_) - p_77615_4_;
 
 		ArrowLooseEvent event = new ArrowLooseEvent(p_77615_3_, p_77615_1_, j);
 		MinecraftForge.EVENT_BUS.post(event);
-		if (event.isCanceled())
-		{
+		if (event.isCanceled()) {
 			return;
 		}
 		j = event.charge;
 
 		boolean flag = p_77615_3_.capabilities.isCreativeMode || EnchantmentHelper.getEnchantmentLevel(Enchantment.infinity.effectId, p_77615_1_) > 0;
 
-		if (flag || p_77615_3_.inventory.hasItem(Items.arrow))
-		{
-			float f = (float)j / 20.0F;
+		if (flag || p_77615_3_.inventory.hasItem(Items.arrow)) {
+			float f = (float) j / 20.0F;
 			f = (f * f + f * 2.0F) / 3.0F;
 
-			if ((double)f < 0.1D)
-			{
+			if ((double) f < 0.1D) {
 				return;
 			}
 
-			if (f > 1.0F)
-			{
+			if (f > 1.0F) {
 				f = 1.0F;
 			}
 
 			EntityFlyingCarrot entityFlyingCarrot = new EntityFlyingCarrot(p_77615_2_, p_77615_3_, f * 2.0F);
 
-			if (f == 1.0F)
-			{
+			if (f == 1.0F) {
 				entityFlyingCarrot.setIsCritical(true);
 			}
 
 			int k = EnchantmentHelper.getEnchantmentLevel(Enchantment.power.effectId, p_77615_1_);
 
-			if (k > 0)
-			{
-				entityFlyingCarrot.setDamage(entityFlyingCarrot.getDamage() + (double)k * 0.5D + 0.5D);
+			if (k > 0) {
+				entityFlyingCarrot.setDamage(entityFlyingCarrot.getDamage() + (double) k * 0.5D + 0.5D);
 			}
 
 			int l = EnchantmentHelper.getEnchantmentLevel(Enchantment.punch.effectId, p_77615_1_);
 
-			if (l > 0)
-			{
+			if (l > 0) {
 				entityFlyingCarrot.setKnockbackStrength(l);
 			}
 
-			if (EnchantmentHelper.getEnchantmentLevel(Enchantment.flame.effectId, p_77615_1_) > 0)
-			{
+			if (EnchantmentHelper.getEnchantmentLevel(Enchantment.flame.effectId, p_77615_1_) > 0) {
 				entityFlyingCarrot.setFire(100);
 			}
 
 			p_77615_1_.damageItem(1, p_77615_3_);
 			p_77615_2_.playSoundAtEntity(p_77615_3_, "random.bow", 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
 
-			if (flag)
-			{
+			if (flag) {
 				entityFlyingCarrot.canBePickedUp = 2;
-			}
-			else
-			{
+			} else {
 				p_77615_3_.inventory.consumeInventoryItem(Items.arrow);
 			}
 
-			if (!p_77615_2_.isRemote)
-			{
+			if (!p_77615_2_.isRemote) {
 				p_77615_2_.spawnEntityInWorld(entityFlyingCarrot);
 			}
 		}
+	}
 }
