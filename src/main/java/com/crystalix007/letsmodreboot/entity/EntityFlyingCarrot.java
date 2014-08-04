8f1e2ba967213c418fb3eebc09a82c6fd02d9111
michaelkuc6 <michaelkuc6@gmail.com>
2014-08-04 18:30:36 +0100

    Added base FlyingCarrot class
    Added base class, FlyingCarrot

---
+++ src/main/java/com/crystalix007/letsmodreboot/entity/EntityFlyingCarrot.java
@@ -0,0 +1,13 @@
+package com.crystalix007.letsmodreboot.entity;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.world.World;
+
+public class EntityFlyingCarrot extends EntityArrow
+{
+	public EntityFlyingCarrot(World world, EntityLivingBase entityLivingBase, float velocity)
+	{
+		super(world, entityLivingBase, velocity);
+	}
+}
