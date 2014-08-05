2003a8435a9429cb05cc7b8b47815e49770df0d4
olizmc <olizmc@gmail.com>
2014-08-05 15:41:32 +0100

    Removed Clearglass from records

--- src/main/java/com/crystalix007/letsmodreboot/init/ModBlocks.java
+++ src/main/java/com/crystalix007/letsmodreboot/init/ModBlocks.java
@@ -10,13 +10,11 @@ public class ModBlocks
 	public static final BlockLMRB flag = new BlockFlag();
 	public static final BlockLMRB spinel = new BlockSpinel();
 	public static final BlockLMRB siliconNitride = new BlockSiliconNitride();
-    public static final BlockLMRB clearGlass = new BlockClearGlass();
 
 	public static void init()
 	{
 		GameRegistry.registerBlock(flag, "flag");
 		GameRegistry.registerBlock(spinel, "spinel");
 		GameRegistry.registerBlock(siliconNitride, "siliconNitride");
-        GameRegistry.registerBlock(clearGlass, "clearGlass");
     }
 }
