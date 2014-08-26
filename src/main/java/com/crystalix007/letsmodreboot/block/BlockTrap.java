050b4e9a472818e3f442afdac444e7e34835b49d
olizmc <olizmc@gmail.com>
2014-08-26 10:39:03 +0100

    Added trap block
    Still under developement

---
+++ src/main/java/com/crystalix007/letsmodreboot/block/BlockTrap.java
@@ -0,0 +1,9 @@
+package com.crystalix007.letsmodreboot.block;
+
+public class BlockTrap extends BlockGlassLMRB {
+    public BlockTrap() {
+        super("trapBlock");
+        this.setBlockName("trapBlock");
+        this.lightOpacity = 0;
+    }
+}
