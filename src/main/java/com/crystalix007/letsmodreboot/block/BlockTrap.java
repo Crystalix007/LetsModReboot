f183e657529d384df91f2a4dafa7b54b8ccf391f
olizmc <olizmc@gmail.com>
2014-08-26 10:50:59 +0100

    Removed Bounding Box for Trap Block

--- src/main/java/com/crystalix007/letsmodreboot/block/BlockTrap.java
+++ src/main/java/com/crystalix007/letsmodreboot/block/BlockTrap.java
@@ -1,5 +1,10 @@
 package com.crystalix007.letsmodreboot.block;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.World;
+
 public class BlockTrap extends BlockGlassLMRB {
     public BlockTrap() {
         super("trapBlock");
@@ -7,3 +12,10 @@ public class BlockTrap extends BlockGlassLMRB {
         this.lightOpacity = 0;
     }
 }
+
+@SideOnly(Side.CLIENT)
+
+public AxisAlignedBB getSelectedBoundingBoxFromPool(World world, int x, int y, int z)
+    {
+        return AxisAlignedBB.getBoundingBox(0, 0, 0, 0, 0, 0);
+    }
