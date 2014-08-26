e20860aa371875f950379d71748b32c16c344a3e
michaelkuc6 <michaelkuc6@gmail.com>
2014-08-26 10:22:25 +0100

    Adding colour manipulation
    Using RGB values and dynamic colour values in-game, progress has been
    made to allow in-game colouring of Invisiglass.

--- src/main/java/com/crystalix007/letsmodreboot/block/BlockInvisiGlass.java
+++ src/main/java/com/crystalix007/letsmodreboot/block/BlockInvisiGlass.java
@@ -1,7 +1,12 @@
 package com.crystalix007.letsmodreboot.block;
 
+import com.crystalix007.letsmodreboot.proxy.ClientProxy;
+import com.crystalix007.letsmodreboot.utility.LogHelper;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemDye;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 
@@ -9,6 +14,9 @@ import java.util.Random;
 
 public class BlockInvisiGlass extends BlockGlassLMRB
 {
+	protected int red = 0;
+	protected int green = 0;
+	protected int blue = 0;
 
 	public BlockInvisiGlass()
 	{
@@ -22,6 +30,42 @@ public class BlockInvisiGlass extends BlockGlassLMRB
 	{
 		return null;
 	}*/
+
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int i, float v, float v1, float v2)
+	{
+		ItemStack currentItem = player.getCurrentEquippedItem();
+		if (currentItem == null)
+			return false;
+
+		LogHelper.info("Current item: " + currentItem.getUnlocalizedName());
+		LogHelper.info("ItemDye: " + new ItemDye().getUnlocalizedName());
+
+		if (currentItem.getUnlocalizedName().equals(new ItemDye().getUnlocalizedName()))
+		{
+			if (world.isRemote)
+				ClientProxy.printMessageToPlayer("Using dye on block");
+			if (currentItem.getItemDamage() == 1 && red < 240)
+				red += 20;
+			if (currentItem.getItemDamage() == 2 && green < 240)
+				green += 20;
+			if (currentItem.getItemDamage() == 4 && blue < 240)
+				blue += 20;
+		}
+
+		if (world.isRemote)
+		{
+			ClientProxy.printMessageToPlayer("Red: " + String.valueOf(red));
+			ClientProxy.printMessageToPlayer("Green: " + String.valueOf(green));
+			ClientProxy.printMessageToPlayer("Blue: " + String.valueOf(blue));
+		}
+		return super.onBlockActivated(world, x, y, z, player, i, v, v1, v2);
+	}
+
+	@SideOnly(Side.CLIENT)
+	public int getBlockColor()
+	{
+		return ((red * 65536) + (green * 256) + blue);
+	}
 
 	@Override
 	public int quantityDropped(Random random) {
